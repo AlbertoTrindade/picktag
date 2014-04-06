4c4d57f72b6ad2d8740a5f65d69293e1c2a10b8a
Alberto Trindade <att@cin.ufpe.br>
2014-04-06 18:44:00 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading) { 'PickTag' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    let(:heading) { 'About' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
+  end
+
+end
