515f37d76733668d268e75154581824175a2c5a7
Alberto Trindade <att@cin.ufpe.br>
2014-04-06 19:09:34 -0500

    Finish initial layout and bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,4 +27,13 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Home"
+    click_link "picktag"
+    expect(page).to have_title(full_title(''))
+  end
+
 end
