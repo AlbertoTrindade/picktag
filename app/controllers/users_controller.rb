2246eba3d39617db9bc80a134b5125148938b2a5
Alberto Trindade <att@cin.ufpe.br>
2014-04-07 16:48:48 -0500

    Finish user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
       flash[:success] = "Welcome to the PickTag!"
       redirect_to @user
     else
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,8 +20,8 @@ describe "User pages" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Password",     with: "12345678"
+        fill_in "Confirmation", with: "12345678"
       end
 
       it "should create a user" do
