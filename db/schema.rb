4c5d7051899c85f7280fc779dc24f3e575e8e03a
Pericles Alves <periclesroalves@gmail.com>
2014-04-28 20:18:26 -0500

    Fnished signin and signout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407175454) do
+ActiveRecord::Schema.define(version: 20140428195432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,12 @@ ActiveRecord::Schema.define(version: 20140407175454) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "remember_token"
     t.float    "reputation"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Joao da Silva"
+    email    "joao.da.silva@example.com"
+    password "12345678"
+    password_confirmation "12345678"
+  end
+end
